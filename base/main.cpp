eb1da7d7779d6b09cbfe5e0349f30ac669102141
Maksim Belfer <Maxbelf1@gmail.com>
2023-10-27 21:56:17 +0300

    initial

---
+++ base/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  base
+//
+//  Created by Максим Бельфер on 27.10.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
